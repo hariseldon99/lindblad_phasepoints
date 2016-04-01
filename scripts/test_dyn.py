8385e48ef73798a0775cbdb306e17287ec836743
hariseldon99 <hariseldon99@gmail.com>
2016-04-01 17:46:13 +0200

    m

--- scripts/test_dyn.py
+++ scripts/test_dyn.py
@@ -13,11 +13,23 @@ def run_lb():
   size = comm.Get_size()
 
   #Parameters
-  lattice_size = 10
+  lattice_size = 5
   l = lattice_size
   amp = 40.0
-  det = 3.0
+  det = 0.0
   rad = 3.5
+  
+  X = np.multiply(1.0,[0.8433750829,-1.2803473026,1.0181477728,\
+  0.1174184381,0.3337908024])
+  Y = np.multiply(1.0,[0.9357690701,1.1295487005,0.2797869521,\
+  -1.6137283835,-0.6879217930])
+  Z = np.multiply(1.0,[-0.6672270133,-0.9366718440,-1.6653795701,\
+  -0.9979258426,1.2716974002])
+
+  c = np.vstack((X,Y,Z)).T
+  
+  a = np.array([lb.Atom(coords = c[i], index = i) for i in xrange(l)])
+
   thetas = np.array([0.0,np.pi/4.])
   kx = np.sin(thetas)
   ky = np.zeros(2)
@@ -28,14 +40,14 @@ def run_lb():
   p = lb.ParamData(latsize=lattice_size, amplitude=amp, detuning=det, cloud_rad=rad, kvecs=momenta)
 
   #Initiate the DTWA system with the parameters 
-  d = lb.BBGKY_System_Noneqm(p, comm, verbose=True)
+  d = lb.BBGKY_System_Noneqm(p, comm, atoms=a, verbose=True)
   #Prepare the times
   t0 = 0.0
-  ncyc = 20.0
-  nsteps = 3000
+  ncyc = 30.0
+  nsteps = 4000
   times = np.linspace(t0, ncyc, nsteps)
   timestep = times[1]-times[0]
-  (corrdata, distribution, atoms_info) = d.evolve(times, nchunks=5)
+  (corrdata, distribution, atoms_info) = d.evolve(times, nchunks=1)
   if rank == 0:  
     print " "
     print "Data of atoms in gas:"
